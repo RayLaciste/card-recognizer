a2e490b2bac1c2725dac4dc9dd4458347c078726
Ray Laciste <raylaciste@yahoo.com>
2025-06-18 19:25:48 -0700

    Drawing Shapes & Putting Text

--- read.py
+++ read.py
@@ -1,7 +1,16 @@
 import cv2 as cv
 
 img = cv.imread('images/sv2_en_001.jpg')
+# cv.imshow('Card', img)
 
-cv.imshow('Card', img)
+def rescaleFrame(frame, scale=0.5):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+    
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
-cv.waitKey(0)
+img_resized = rescaleFrame(img)
+cv.imshow('Card', img_resized)
+
+cv.waitKey(0) 
